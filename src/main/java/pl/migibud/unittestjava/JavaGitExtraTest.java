0563e0469d73f0bb27b8147020799c3392aee295
Piotr Migaj praca <piotr.migaj@pg-projekt.pl>
2022-04-01 08:39:46 +0200

    Nowy testowy plik GITa dla nowej gałezi

---
+++ src/main/java/pl/migibud/unittestjava/JavaGitExtraTest.java
@@ -0,0 +1,9 @@
+package pl.migibud.unittestjava;
+
+public class JavaGitExtraTest {
+
+    public static void main(String[] args) {
+        System.out.println("Plik testowy dla nowej gałęzi GITa");
+    }
+
+}
