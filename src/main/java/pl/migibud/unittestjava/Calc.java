fe0fd8ea5806b98d04caabdf24b4f58357f72dad
Piotr Migaj praca <piotr.migaj@pg-projekt.pl>
2022-04-05 17:16:36 +0200

    End of first unit test

--- src/main/java/pl/migibud/unittestjava/Calc.java
+++
@@ -1,9 +0,0 @@
-package pl.migibud.unittestjava;
-
-public class Calc {
-
-    public int sum(int a, int b){
-        return a+b;
-    }
-
-}
--- src/main/java/pl/migibud/unittestjava/JavaGitExtraTest.java
+++
@@ -1,9 +0,0 @@
-package pl.migibud.unittestjava;
-
-public class JavaGitExtraTest {
-
-    public static void main(String[] args) {
-        System.out.println("Plik testowy dla nowej gałęzi GITa");
-    }
-
-}
